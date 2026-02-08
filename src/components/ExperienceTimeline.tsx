551f028ebcbaccdd2ca1abf0e4c9d60f3c4a6571
Nash Nziramasanga <nash@linktr.ee>
2026-02-08 22:29:32 +1100

    added icons

--- src/components/ExperienceTimeline.tsx
+++ src/components/ExperienceTimeline.tsx
@@ -3,34 +3,6 @@ import { cn } from "@/lib/utils";
 
 type Experience = (typeof experiencesData)[number];
 
-function TimelineIcon({
-  type,
-  className,
-}: {
-  type: string;
-  className?: string;
-}) {
-  if (type === "asterisk") {
-    return (
-      <span className={cn("text-lg font-bold text-white", className)}>*</span>
-    );
-  }
-  if (type === "dog") {
-    return (
-      <svg
-        className={cn("h-5 w-5 text-white", className)}
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        aria-hidden
-      >
-        {/* Simple sitting dog silhouette */}
-        <path d="M12 4c-1.8 0-3.4 1.2-3.9 2.9-.3-.5-.8-.9-1.4-1.1-1-.4-2-.2-2.7.5C3.5 7.2 3 8.4 3 9.8v6.5c0 1.6 1.3 2.9 2.9 2.9h2.2v2.2h1.8v-2.2h2.2v2.2h1.8v-2.2h2.2c1.6 0 2.9-1.3 2.9-2.9V9.8c0-1.4-.5-2.6-1.4-3.5-.7-.7-1.7-1-2.7-.5-.6.2-1.1.6-1.4 1.1C15.4 5.2 13.8 4 12 4zm-2 9.5c0 .8-.7 1.5-1.5 1.5S7 14.3 7 13.5 7.7 12 8.5 12s1.5.7 1.5 1.5zm5 0c0 .8-.7 1.5-1.5 1.5s-1.5-.7-1.5-1.5.7-1.5 1.5-1.5 1.5.7 1.5 1.5z" />
-      </svg>
-    );
-  }
-  return null;
-}
-
 export function ExperienceTimeline() {
   return (
     <div className="flex min-h-full flex-1 flex-col px-8 py-12">
@@ -47,13 +19,8 @@ export function ExperienceTimeline() {
             className="relative flex items-start gap-6 pb-10 last:pb-0"
           >
             {/* Circle marker */}
-            <div
-              className={cn(
-                "relative z-10 flex h-8 w-8 shrink-0 items-center justify-center rounded-full ring-4 ring-[#1A202C]",
-                exp.iconColor
-              )}
-            >
-              <TimelineIcon type={exp.iconType} />
+            <div className="relative z-10 flex h-8 w-8 shrink-0 items-center justify-center rounded-full ring-4 ring-[#1A202C]">
+              <img src={exp.logo} alt={exp.company} className="h-5 w-5" />
             </div>
             {/* Card */}
             <div className="min-w-0 flex-1 rounded-2xl bg-slate-200 px-5 py-4 shadow-sm">
