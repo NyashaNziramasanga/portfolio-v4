9a6e79eae86e4d4408fd2e36c6845f17af19bc15
Nyasha (Nash) Nziramasanga <nash@linktr.ee>
2026-09-02 17:24:55 +1000

    perf(fonts): load only the Latin Inter subset

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
-import "@fontsource-variable/inter";
 import "./index.css";
 import { routeTree } from "./routeTree.gen";
 
